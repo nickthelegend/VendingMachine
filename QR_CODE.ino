8e6365b98dce94db4cfc9eebadb387d32d318db0
nickthelegend <testingtesla7@gmail.com>
2025-10-18 12:37:05 +0530

    Initial QR Display

---
+++ QR_CODE.ino
@@ -0,0 +1,26 @@
+
+#include <SPI.h>
+#include <TFT_eSPI.h>
+#include <qrcode_espi.h>
+
+TFT_eSPI display = TFT_eSPI();
+QRcode_eSPI qrcode (&display);
+
+
+void setup() {
+
+    Serial.begin(115200);
+    Serial.println("");
+    Serial.println("Starting...");
+
+    // enable debug qrcode
+    // qrcode.debug();
+    display.init();
+    // Initialize QRcode display using library
+    qrcode.init();
+    // create qrcode
+    qrcode.create("Hello world.");
+
+}
+
+void loop() { }
